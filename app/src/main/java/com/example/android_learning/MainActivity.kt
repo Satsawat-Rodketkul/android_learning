6b13417f8f4637a834a6da1a7d773bfc37435c76
satsawat rodketkul <satsawatrodketkul@gmail.com>
2024-06-17 18:23:39 +0700

    update file

--- app/src/main/java/com/example/android_learning/MainActivity.kt
+++ app/src/main/java/com/example/android_learning/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.android_learning.ui.theme.Android_learningTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,16 +34,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    name = "My Preview")
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     Android_learningTheme {
-        Greeting("Android")
+        Greeting("Mango")
     }
 }
